fe8032171e237bcd57ba9036da130a7cf74a3d1c
B3helit <155858361+B3helit@users.noreply.github.com>
2024-09-23 05:58:49 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Banking</title>
+	<?php require 'assets/autoloader.php'; ?>
+	<?php require 'assets/function.php'; ?>
+	<?php
+    $con = new mysqli('localhost','root','','bank_system');
+	$bankname = 'IMAMU BANK';
+    //define('bankname', 'IMAMU Bank',true);
+	
+		$error = "";
+		if(isset($_POST['userRegister']))
+		{
+			$error = "";
+			$user = $_POST['email'];
+		 	$pass = $_POST['password'];
+			$name = $_POST['name'];
+			$number = $_POST['number'];
+			$city = $_POST['city'];
+			$address = $_POST['address'];
+
+			if(!empty($user) && !empty($pass) && !is_numeric($user)){ // method for more security bridges.
+				$result = $con->query("insert into useraccounts (email, password, name, number, city, address) values ($user, $pass, $name, $number, $city, $address)");
+				/*
+email	
+password	
+name	
+balance	
+number	
+city	
+address	
+source	
+accountNo	
+branch	
+accountType	
+date
+*/ 
+				echo "Account created.";
+
+			}else{
+				echo "Please enter some valid information.";
+			}
+		}
+		if (isset($_POST['userLogin']))
+		{
+			$error = "";
+  			$user = $_POST['email'];
+		    $pass = $_POST['password'];
+		   
+		    $result = $con->query("select * from userAccounts where email='$user' AND password='$pass'");
+		    if($result->num_rows>0)
+		    { 
+		      session_start();
+		      $data = $result->fetch_assoc();
+		      $_SESSION['userId']=$data['id'];
+		      $_SESSION['user'] = $data;
+		      header('location:index.php');
+		     }
+		    else
+		    {
+		      $error = "<div class='alert alert-warning text-center rounded-0'>Username or password wrong try again!</div>";
+		    }
+		}
+		if (isset($_POST['cashierLogin']))
+		{
+			$error = "";
+  			$user = $_POST['email'];
+		    $pass = $_POST['password'];
+		   
+		    $result = $con->query("select * from login where email='$user' AND password='$pass'");
+		    if($result->num_rows>0)
+		    { 
+		      session_start();
+		      $data = $result->fetch_assoc();
+		      $_SESSION['cashId']=$data['id'];
+		      //$_SESSION['user'] = $data;
+		      header('location:cindex.php');
+		     }
+		    else
+		    {
+		      $error = "<div class='alert alert-warning text-center rounded-0'>Username or password wrong try again!</div>";
+		    }
+		}
+		if (isset($_POST['managerLogin']))
+		{
+			$error = "";
+  			$user = $_POST['email'];
+		    $pass = $_POST['password'];
+		   
+		    $result = $con->query("select * from login where email='$user' AND password='$pass' AND type='manager'");
+		    if($result->num_rows>0)
+		    { 
+		      session_start();
+		      $data = $result->fetch_assoc();
+		      $_SESSION['managerId']=$data['id'];
+		      //$_SESSION['user'] = $data;
+		      header('location:mindex.php');
+		     }
+		    else
+		    {
+		      $error = "<div class='alert alert-warning text-center rounded-0'>Username or password wrong try again!</div>";
+		    }
+		}
+
+	 ?>
+</head>
+<body style="background: url(images/bg-login2.jpg);background-size: 100%">
+<h1 class="alert alert-success rounded-0"><?php echo $bankname; ?><small class="float-right text-muted" style="font-size: 12pt;"><kbd></kbd></small></h1>
+<br>
+<?php echo $error ?>
+<br>
+<div id="accordion" role="tablist" class="w-25 float-right shadowBlack" style="margin-right: 222px">
+	<br><h4 class="text-center text-white">Select Your Session</h4>
+  <div class="card rounded-0">
+    <div class="card-header" role="tab" id="headingOne">
+      <h5 class="mb-0">
+        <a style="text-decoration: none;" data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+         <button class="btn btn-outline-success btn-block">User Login</button>
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+       <form method="POST">
+       	<input type="email" value="some@gmail.com" name="email" class="form-control" required placeholder="Enter Email">
+       	<input type="password" name="password" value="some" class="form-control" required placeholder="Enter Password">
+       	<button type="submit" class="btn btn-primary btn-block btn-sm my-1" name="userLogin">Enter </button>
+       </form>
+      </div>
+    </div>
+  </div>
+  <div class="card rounded-0">
+    <div class="card-header" role="tab" id="headingTwo">
+      <h5 class="mb-0">
+        <a class="collapsed btn btn-outline-success btn-block" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Manager Login
+        </a>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div class="card-body">
+         <form method="POST">
+       	<input type="email" value="manager@manager.com" name="email" class="form-control" required placeholder="Enter Email">
+       	<input type="password" name="password" value="manager" class="form-control" required placeholder="Enter Password">
+       	<button type="submit" class="btn btn-primary btn-block btn-sm my-1" name="managerLogin">Enter </button>
+       </form>
+      </div>
+    </div>
+  </div>
+  <div class="card rounded-0">
+    <div class="card-header" role="tab" id="headingThree">
+      <h5 class="mb-0">
+        <a class="collapsed btn btn-outline-success btn-block" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+         Cashier Login
+        </a>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+      <div class="card-body">
+        <form method="POST">
+       	<input type="email" value="cashier@cashier.com" name="email" class="form-control" required placeholder="Enter Email">
+       	<input type="password" name="password" value="cashier" class="form-control" required placeholder="Enter Password">
+       	<button type="submit"  class="btn btn-primary btn-block btn-sm my-1" name="cashierLogin">Enter </button>
+       </form>
+      </div>
+    </div>
+  </div>
+  <div class="card rounded-0">
+    <div class="card-header" role="tab" id="headingFour">
+      <h5 class="mb-0">
+        <a class="collapsed btn btn-outline-success btn-block" data-toggle="collapse" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+         User Register
+        </a>
+      </h5>
+    </div>
+    <div id="collapseFour" class="collapse" role="tabpanel" aria-labelledby="headingFour" data-parent="#accordion">
+      <div class="card-body">
+        <form method="POST">
+       	<input type="email" value="random@register.com" name="email" class="form-control" required="" placeholder="Enter Email">
+		<input type="name" value="" name="name" class="form-control" required="" placeholder="Enter Name">
+       	<input type="number" value="050000000000" name="number" class="form-control" required="" placeholder="Enter Phone number">
+       	<input type="city" value="Riyadh" name="city" class="form-control" required="" placeholder="Enter City">
+       	<input type="address" value="Imam Mohammed University" name="address" class="form-control" required="" placeholder="Enter Your Address">
+
+       	<input type="password" name="password" value="random" class="form-control" required="" placeholder="Enter Password">
+       	<button type="submit" class="btn btn-primary btn-block btn-sm my-1" name="userRegister">Create </button>
+       </form>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
